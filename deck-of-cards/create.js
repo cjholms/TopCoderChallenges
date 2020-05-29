1fea3276043bc2a0a54fe69e80a5f6afa82c6e4a
Carol Holmstedt <cjkulzer@gmail.com>
2020-05-29 09:12:29 -0700

    Re-factor to one line

--- deck-of-cards/create.js
+++ deck-of-cards/create.js
@@ -1,25 +1,8 @@
 // Testing use of map function
-// Objective: Create a deck of cards using the map function
+// Objective: Use one line of code to create a deck of cards using the map function
 
 const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 const members = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
 
-let deck = [];
-
-// function that generates all the cards in a suit and adds
-// the cards to the deck
-let addSuitToDeck = (suit) => {
-    return members.map(m => deck.push(m + " of " + suit));
-}
-
-// probably don't need an addSuitToDeck function, but created one to make the line
-// below easier to read
-suits.map(addSuitToDeck);
-
-console.log(deck);
-
-// for (let suit of suits) {
-//     for (let member of members) {
-//         deck.push(member + " of " + suit);
-//     }
-// }
+// reduce function is used to flatten the array of arrays
+console.log(suits.map(suit => members.map(m => m + " of " + suit)).reduce((acc, val) => acc.concat(val), []));
